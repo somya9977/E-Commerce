8a2c22a04141b3e2648b4d2d6b1d7c17767161fd
somya Thakur <somyathakur040403@gmail.com>
2026-02-14 12:29:28 +0530

    Initial React Vite E-Commerce project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/E-Commerce-/'   // <-- Add this line
+  base: '/E-Commerce/'   // <-- Add this line
 });
 
